cdf02ac6d8ffbd06456f0b6a5b21600f8e8b971b
Lendysx16 <91876886+Lendysx16@users.noreply.github.com>
2023-02-26 15:55:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("Calculator")
 
 from Calculator.calculator import Calculator
-from Calculator.matherror import ParenthesisError, InputExpressionError
+
 
 calculator_ = Calculator()
 calculator_.load_new_expression(input())
